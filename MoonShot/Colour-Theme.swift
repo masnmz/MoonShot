7635d62211318eef8a77ddb0f744112cbc0a8c89
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-04 18:12:04 +0100

    Adding Images of missions, date for NavigationLink. Custom Background Colour.

---
+++ MoonShot/Colour-Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Colour-Theme.swift
+//  MoonShot
+//
+//  Created by Mehmet Alp Sönmez on 04/06/2024.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackGround: Color {
+        Color(red:0.1, green: 0.1, blue: 0.2)
+    }
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     var body: some View {
-        Text(String(astronauts.count))
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                                    .padding()
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                }
+                                .padding()
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                                )
+                           
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackGround)
+            .preferredColorScheme(.dark)
+        }
     }
 }
 
