5bbd529088b9194a37ae739481f83bdaa38e8ba5
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-04 23:01:27 +0100

    Mission View, Astronaut Images

---
+++ MoonShot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  MoonShot
+//
+//  Created by Mehmet Alp Sönmez on 04/06/2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
