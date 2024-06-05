d7c8539e16a9535bec2e1fa54c1f85226e123841
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-06-05 15:59:54 +0100

    Switch Between List Layout and Grid Layout

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions.json")
+struct GridView: View {
+    let astronauts: [String:Astronaut]
+    let missions: [Mission]
     
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -44,19 +45,92 @@ struct ContentView: View {
                             .overlay(
                                 RoundedRectangle(cornerRadius: 10)
                                     .stroke(.lightBackground)
-                                )
-                           
+                            )
+                            
                         }
                     }
                 }
                 .padding([.horizontal, .bottom])
             }
-            .navigationTitle("Moonshot")
-            .background(.darkBackGround)
-            .preferredColorScheme(.dark)
         }
     }
 }
+
+struct ListView: View {
+    let astronauts: [String:Astronaut]
+    let missions: [Mission]
+    
+    var body: some View {
+        List {
+            ScrollView {
+                LazyVStack {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: .infinity, height: 100)
+                                    .padding()
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                }
+                                .padding()
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                            
+                        }
+                    }
+                }
+                .padding(.bottom)
+            }
+            .listRowInsets(EdgeInsets())
+            .listRowBackground(Color.darkBackGround)
+            .background(.darkBackGround)
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ContentView: View {
+    @State private var listView = false
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationStack {
+            Group {
+                if listView {
+                    ListView(astronauts: astronauts, missions: missions)
+                } else {
+                    GridView(astronauts: astronauts, missions: missions)
+                }
+            }
+                .navigationTitle("Moonshot")
+                .background(.darkBackGround)
+                .preferredColorScheme(.dark)
+                .toolbar {
+                    Button(listView ? "Grid View" : "List View") {
+                        listView.toggle()
+                    }
+                }
+            }
+        }
+    }
 
 #Preview {
     ContentView()
